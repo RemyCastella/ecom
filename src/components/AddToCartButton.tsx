fc058cffd6edd40fb8737b64c8b40367662ce5f8
Remy Castella <remy.jpen@gmail.com>
2025-05-22 17:38:01 +0900

    refactor components/AddToCartButton

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
+import { ProductData } from "../types/products";
 import { FaCartPlus } from "react-icons/fa";
 import addToCart from "../functions/addToCart";
 import { Toaster, toast } from "sonner";
 
-export default function AddToCartButton({ products, productId, setCart }) {
+interface AddToCartButtonProps {
+  products: ProductData[],
+  productId: number,
+  setCart: Dispatch<SetStateAction<number[]>>
+}
+
+export default function AddToCartButton({ products, productId, setCart }: AddToCartButtonProps) {
   return (
     <div
       className="mt-4 flex w-36 cursor-pointer items-center bg-cyan-100 p-2 text-lg"
