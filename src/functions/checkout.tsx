86cba1381e00198b1dd334f405af0d432a0f6ea8
Remy Castella <remy.jpen@gmail.com>
2025-05-22 17:00:08 +0900

    refactor functions/checkout

--- src/functions/checkout.tsx
+++ src/functions/checkout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Confetti } from "react-confetti";
+import Confetti from "react-confetti";
 
 export default function checkout() {
   alert("Thanks for shopping!");
